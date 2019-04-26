73f18e0efd58639337d2c893033fad90f1aa479a
Philip Wang <RainBoltz@users.noreply.github.com>
2019-04-26 23:25:45 +0800

    update-to-date
    wtf appledaily updated their blocks...

--- appledaily_paywall_remover/auto_remove.js
+++ appledaily_paywall_remover/auto_remove.js
@@ -1,10 +1,37 @@
 
 console.log( "Script properly injected into page" );
 
-let paywall = document.querySelector( '.ndPaywall' );
-let article = document.querySelector( '.ndArticle_content > div' );
 
-paywall.style.display = "none";
-article.style.display = "block";
+function updateIframe() {
+  $('#hack_frame').contents().find("head").append($("<link/>", 
+    { rel: "stylesheet", href: "https://github.com/RainBoltz/appledaily_paywall_remover/blob/master/appledaily_paywall_remover/appledaily_old_style.css", type: "text/css" }
+  ));
+
+  $('#hack_frame').contents().find("body").addClass('ndArticle_content').css("margin", "0px");
+
+  setTimeout(function(){
+    if($("#hack_frame").length > 0) {
+      $("#hack_frame")[0].height = "";
+      $("#hack_frame")[0].height = $("#hack_frame")[0].contentWindow.document.body.scrollHeight + "px";
+    }   
+  },50);
+}
+
+$.get(window.location.href, function(data) {
+  var news_content = $(data).find(".ndArticle_margin")[0].innerHTML;
+
+  $(".ndPaywall").remove();
+  $('<iframe id="hack_frame" name="hack_frame" onload="updateIframe()" style="width: 100%">').appendTo("hgroup").ready(function(){
+    setTimeout(function(){
+        $("#hack_frame").contents().find("body").append(news_content);
+        console.log( "100vh gogogo!" );
+        $("#hack_frame").height("100vh");
+    },50);
+  });
+});
+
 
 console.log( "DONE!" );
+
+
+
